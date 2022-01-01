ffe1a977a3ac452c706cb715853622463083a3a5
MD Jannatin Naim <mail.jnaim@gmail.com>
2022-01-01 23:41:51 +0600

    base

---
+++ app.js
@@ -0,0 +1,21 @@
+// * Channel Names e.g ['jannatinnaim', 'twitch']
+const channels = ['jannatinnaim'];
+// * Notification Sound e.g './notification.mp3' / 'sound.mp3'
+const notification = './notification.mp3';
+
+/*
+    ! DO NOT MODIFY THE CODE BELOW!
+*/
+
+// Create TMI Client
+const tmiClient = new tmi.Client({ channels });
+// Create Audio Object
+const audio = new Audio(notification);
+
+// Connect To Twitch Channels
+tmiClient.connect();
+// On Message Play Sound
+tmiClient.on('message', () => {
+    audio.play();
+    console.log('MESSAGE');
+});
