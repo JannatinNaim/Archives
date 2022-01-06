19b3643902e12ff3614fd2a930a07f98f86c4e3f
MD Jannatin Naim <mail.jnaim@gmail.com>
2022-01-06 20:58:08 +0600

    added ability to listen to multiple channels

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -1,21 +1,54 @@
 // * Channel Names e.g ['jannatinnaim', 'twitch']
-const channels = ['jannatinnaim'];
+let channels = [];
 // * Notification Sound e.g './notification.mp3' / 'sound.mp3'
 const notification = '../src/audio/notification.mp3';
 
 /*
     ! DO NOT MODIFY THE CODE BELOW!
 */
-
-// Create TMI Client
-const tmiClient = new tmi.Client({ channels });
-// Create Audio Object
-const audio = new Audio(notification);
-
-// Connect To Twitch Channels
-tmiClient.connect();
-// On Message Play Sound
+let tmiClient = new tmi.Client({
+    channels,
+    options: {
+        debug: true,
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true,
+    },
+});
 tmiClient.on('message', () => {
     audio.play();
     console.log('MESSAGE');
+});
+tmiClient.on('connect', () => {
+    console.log('CONNECTED');
+});
+tmiClient.connect();
+// Create TMI Client
+// Create Audio Object
+const audio = new Audio(notification);
+
+// On Message Play Sound
+
+const channelNameInput = document.getElementById('channel_name_input');
+const addChannelButton = document.getElementById('add_channel_button');
+const channelsList = document.getElementById('channels_list');
+
+addChannelButton.addEventListener('click', async (event) => {
+    const newChannel = document.createElement('li');
+    newChannel.innerText = channelNameInput.value;
+    channelNameInput.value = '';
+
+    for (let i = 0; i < channelsList.children.length; i++) {
+        const item = channelsList.children[i];
+        if (item.innerText === newChannel.innerText) return;
+    }
+    channelsList.appendChild(newChannel);
+
+    for (let i = 0; i < channelsList.children.length; i++) {
+        const item = channelsList.children[i];
+        tmiClient.channels.push(item.innerText);
+    }
+    await tmiClient.disconnect();
+    await tmiClient.connect();
 });
