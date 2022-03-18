a948f8ec2390a64b75149a6d45562ef390bc3b2d
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-03-18 21:50:07 +0600

    [feat] functions/log (#8)
    Logging module for beautiful console logs.

---
+++ src/functions/log.js
@@ -0,0 +1,61 @@
+const moment = require('moment');
+
+
+/**
+ * Log function with categories and types.
+ * @param {String} message Message to log.
+ * @param {String} logType Type of log.
+ * @param {String} processType Type of process.
+ * @param {String} logCategory Type of category.
+ */
+async function log(
+    message,
+    logType = 'info',
+    processType = 'general',
+    logCategory = 'base',
+) {
+  const TIMESTAMP = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  const logTypeConfig = {
+    info: {color: ''},
+    success: {color: 'green'},
+    warn: {color: 'yellow'},
+    error: {color: 'red'},
+    debug: {color: 'magenta'},
+  };
+
+  const logCategoryConfig = {
+    event: {color: 'yellow'},
+  };
+
+  const processTypeConfig= {
+    node: {color: 'green'},
+    discord: {color: 'blue'},
+  };
+
+  const logTypeColor = logTypeConfig[logType]?.color || '';
+  const processTypeColor = processTypeConfig[processType]?.color || '';
+  const logCategoryColor = logCategoryConfig[logCategory]?.color || '';
+
+  logType = logTypeColor ?
+    logType.toUpperCase()[logTypeColor] :
+    logType.toUpperCase();
+
+  processType = processTypeColor ?
+    processType.toUpperCase()[processTypeColor] :
+    processType.toUpperCase();
+
+  logCategory = logCategoryColor ?
+    logCategory.toUpperCase()[logCategoryColor] :
+    logCategory.toUpperCase();
+
+  console.log(`[${TIMESTAMP}]`,
+      `[${processType}]`,
+      `[${logType}]`,
+      `[${logCategory}]`,
+      message,
+  );
+};
+
+
+module.exports = log;
