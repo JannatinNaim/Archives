4c92e59f4223196db19b8565d17c5a2651b990fe
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-03-19 16:41:21 +0600

    [feat] functions/getFileNameAndCategory (#18)
    Get file name and category from file path.

---
+++ src/functions/getFileNameAndCategory.js
@@ -0,0 +1,16 @@
+/**
+ * Get file name and category from file path.
+ * @param {String} filePath File path.
+ * @return {Array} Array of file name and category.
+ */
+function getFileNameAndCategory(filePath) {
+  const filePathArray = filePath.split('/');
+
+  const fileName = filePathArray[filePathArray.length - 1];
+  const fileCategory = filePathArray[filePathArray.length - 2];
+
+  return [fileName, fileCategory];
+}
+
+
+module.exports = getFileNameAndCategory;
